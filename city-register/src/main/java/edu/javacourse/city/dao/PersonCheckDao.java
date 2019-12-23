67bf5a023c91b6aacb684aa98aea599ab8bad799
alnivv <alnivv@gmail.com>
2019-12-23 10:18:42 +0300

    Student project

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -1,4 +1,42 @@
 package edu.javacourse.city.dao;
 
+import edu.javacourse.city.dao.domain.PersonRequest;
+import edu.javacourse.city.dao.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class PersonCheckDao {
+    private static final String SQL_REQUEST = "";
+
+    public PersonResponse checkPerson (PersonRequest request) throws PersonCheckException {
+        PersonResponse response = new PersonResponse();
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(SQL_REQUEST)) {
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                response.setRegistered(true);
+                response.setTemporal(rs.getBoolean("temporal"));
+            }
+
+        }
+        catch (SQLException ex) {
+            throw new PersonCheckException(ex);
+
+        }
+
+        return response;
+    }
+
+    private Connection getConnection() {
+        return null;
+    }
+
 }
+
