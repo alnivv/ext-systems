fec9f55a920104936618c2149a76feb9c2ec2849
Aleksandr Vdovin <alnivv@gmail.com>
2020-01-07 18:19:52 +0300

    Trying to integrate Spring Framework with XML-file

--- register-office/src/main/java/edu/javacourse/register/view/MarriageResponse.java
+++ register-office/src/main/java/edu/javacourse/register/view/MarriageResponse.java
@@ -5,21 +5,4 @@ import java.time.LocalDate;
 
 public class MarriageResponse implements Serializable {
 
-    private String husbandSurename;
-    private String husbandGivenname;
-    private String husbandPatronymic;
-    private LocalDate husbandDateBirth;
-    private String husbandPassportSeria;
-    private String husbandPassportNumber;
-    private LocalDate husbandPassportIssueDate;
-    private String wifeSurename;
-    private String wifeGivenname;
-    private String wifePatronymic;
-    private LocalDate wifeDateBirth;
-    private String wifePassportSeria;
-    private String wifePassportNumber;
-    private LocalDate wifePassportIssueDate;
-
-    private String marriageCertificateNumber;
-    private LocalDate marriageCertificateDate;
 }
